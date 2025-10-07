67092d9c04ea9ef5aac65793aafd388c9ada81e5
Zsomething <nothanks@ontheemail.com>
2025-10-07 10:08:12 -0600

    remove unnecessary generic-ness over the benchmarked function, add "0-threaded" version that doesn't spawn any thread.

--- benches/tangotb.rs
+++ benches/tangotb.rs
@@ -6,7 +6,7 @@ use std::thread;
 
 use std::sync::Arc;
 
-fn gen_mt_bencher</*T, */F>(f: F, num_threads: u64, num_iters: u64) -> impl FnMut(Bencher) -> Box<dyn ErasedSampler>
+fn gen_mt_bencher_generic<F>(f: F, num_threads: u64, num_iters: u64) -> impl FnMut(Bencher) -> Box<dyn ErasedSampler>
 where
     F: Fn(u64) + Send + Sync + 'static
 {
@@ -28,6 +28,30 @@ where
     }
 }
 
+fn gen_mt_bencher_dummyfunc(num_threads: u64, num_iters: u64) -> impl FnMut(Bencher) -> Box<dyn ErasedSampler>
+{
+    move |b: Bencher| {
+        b.iter(move || {
+            thread::scope(|scope| {
+                for _t in 0..num_threads {
+                    scope.spawn(move || {
+                        help_test_dummy_func(num_iters);
+                    });
+                }
+            });
+        })
+    }
+}
+
+fn gen_st_bencher(num_iters: u64) -> impl FnMut(Bencher) -> Box<dyn ErasedSampler>
+{
+    move |b: Bencher| {
+        b.iter(move || {
+            help_test_dummy_func(num_iters);
+        })
+    }
+}
+    
 use std::hint::black_box;
 #[inline(never)]
 pub fn dummy_func(maxi: u8, maxj: u8) -> u8 {
@@ -43,31 +67,136 @@ pub fn dummy_func(maxi: u8, maxj: u8) -> u8 {
 
 pub fn help_test_dummy_func(iters: u64) {
     for _i in 0..iters {
-        dummy_func(9, 7); // This crashed with heap corruption 3 times out of about 20 runs.
-        //dummy_func(2, 3);
+        //dummy_func(9, 7); // This crashed with heap corruption 3 times out of about 20 runs.
+        dummy_func(2, 3);
     }
 }
 
 fn tangotb_benchmarks() -> impl IntoBenchmarks {
     [
+        benchmark_fn("0-threads-1-iters",
+                     gen_st_bencher(1)
+        ),
+        benchmark_fn("0-threads-10-iters",
+                     gen_st_bencher(10)
+        ),
+        benchmark_fn("0-threads-100-iters",
+                     gen_st_bencher(100)
+        ),
+        benchmark_fn("0-threads-1K-iters",
+                     gen_st_bencher(1000)
+        ),
+        benchmark_fn("0-threads-100K-iters",
+                     gen_st_bencher(100_000)
+        ),
+        benchmark_fn("0-threads-1M-iters",
+                     gen_st_bencher(1_000_000)
+        ),
+        benchmark_fn("1-threads-1-iters",
+                     gen_mt_bencher_dummyfunc(1, 1)
+        ),
+        benchmark_fn("1-threads-10-iters",
+                     gen_mt_bencher_dummyfunc(1, 10)
+        ),
+        benchmark_fn("1-threads-100-iters",
+                     gen_mt_bencher_dummyfunc(1, 100)
+        ),
+        benchmark_fn("1-threads-1K-iters",
+                     gen_mt_bencher_dummyfunc(1, 1000)
+        ),
         benchmark_fn("1-threads-100K-iters",
-                     gen_mt_bencher(help_test_dummy_func, 1, 100_000)
+                     gen_mt_bencher_dummyfunc(1, 100_000)
         ),
-        benchmark_fn("32-threads-1M-iters",
-                     gen_mt_bencher(help_test_dummy_func, 32, 1_000_000)
+        benchmark_fn("1-threads-1M-iters",
+                     gen_mt_bencher_dummyfunc(1, 1_000_000)
         ),
-        benchmark_fn("512-threads-1K-iters",
-                     gen_mt_bencher(help_test_dummy_func, 512, 1_000)
+        benchmark_fn("6-threads-1-iters",
+                     gen_mt_bencher_dummyfunc(6, 1)
         ),
-        benchmark_fn("512-threads-10K-iters",
-                     gen_mt_bencher(help_test_dummy_func, 512, 10_000)
+        benchmark_fn("6-threads-10-iters",
+                     gen_mt_bencher_dummyfunc(6, 10)
         ),
-        benchmark_fn("512-threads-100K-iters",
-                     gen_mt_bencher(help_test_dummy_func, 512, 100_000)
+        benchmark_fn("6-threads-100-iters",
+                     gen_mt_bencher_dummyfunc(6, 100)
         ),
-        benchmark_fn("2048-threads-10K-iters",
-                     gen_mt_bencher(help_test_dummy_func, 2048, 10_000)
+        benchmark_fn("6-threads-1K-iters",
+                     gen_mt_bencher_dummyfunc(6, 1000)
         ),
+        benchmark_fn("6-threads-100K-iters",
+                     gen_mt_bencher_dummyfunc(6, 100_000)
+        ),
+        benchmark_fn("6-threads-1M-iters",
+                     gen_mt_bencher_dummyfunc(6, 1_000_000)
+        ),
+        benchmark_fn("12-threads-1-iters",
+                     gen_mt_bencher_dummyfunc(12, 1)
+        ),
+        benchmark_fn("12-threads-10-iters",
+                     gen_mt_bencher_dummyfunc(12, 10)
+        ),
+        benchmark_fn("12-threads-100-iters",
+                     gen_mt_bencher_dummyfunc(12, 100)
+        ),
+        benchmark_fn("12-threads-1K-iters",
+                     gen_mt_bencher_dummyfunc(12, 1000)
+        ),
+        benchmark_fn("12-threads-100K-iters",
+                     gen_mt_bencher_dummyfunc(12, 100_000)
+        ),
+        benchmark_fn("12-threads-1M-iters",
+                     gen_mt_bencher_dummyfunc(12, 1_000_000)
+        ),
+        benchmark_fn("16-threads-1-iters",
+                     gen_mt_bencher_dummyfunc(16, 1)
+        ),
+        benchmark_fn("16-threads-10-iters",
+                     gen_mt_bencher_dummyfunc(16, 10)
+        ),
+        benchmark_fn("16-threads-100-iters",
+                     gen_mt_bencher_dummyfunc(16, 100)
+        ),
+        benchmark_fn("16-threads-1K-iters",
+                     gen_mt_bencher_dummyfunc(16, 1000)
+        ),
+        benchmark_fn("16-threads-100K-iters",
+                     gen_mt_bencher_dummyfunc(16, 100_000)
+        ),
+        benchmark_fn("16-threads-1M-iters",
+                     gen_mt_bencher_dummyfunc(16, 1_000_000)
+        ),
+        benchmark_fn("509-threads-1-iters",
+                     gen_mt_bencher_dummyfunc(509, 1)
+        ),
+        benchmark_fn("509-threads-10-iters",
+                     gen_mt_bencher_dummyfunc(509, 10)
+        ),
+        benchmark_fn("509-threads-100-iters",
+                     gen_mt_bencher_dummyfunc(509, 100)
+        ),
+        benchmark_fn("509-threads-1K-iters",
+                     gen_mt_bencher_dummyfunc(509, 1000)
+        ),
+//xxx        benchmark_fn("509-threads-100K-iters",
+//xxx                     gen_mt_bencher(help_test_dummy_func, 509, 100_000)
+//xxx        ),
+//xxx        benchmark_fn("509-threads-1M-iters",
+//xxx                     gen_mt_bencher(help_test_dummy_func, 509, 1_000_000)
+//xxx        ),
+        // benchmark_fn("32-threads-1M-iters",
+        //              gen_mt_bencher(help_test_dummy_func, 32, 1_000_000)
+        // ),
+        // benchmark_fn("512-threads-1K-iters",
+        //              gen_mt_bencher(help_test_dummy_func, 512, 1_000)
+        // ),
+        // benchmark_fn("512-threads-10K-iters",
+        //              gen_mt_bencher(help_test_dummy_func, 512, 10_000)
+        // ),
+        // benchmark_fn("512-threads-100K-iters",
+        //              gen_mt_bencher(help_test_dummy_func, 512, 100_000)
+        // ),
+        // benchmark_fn("2048-threads-10K-iters",
+        //              gen_mt_bencher(help_test_dummy_func, 2048, 10_000)
+        // ),
    ]
 }
 
