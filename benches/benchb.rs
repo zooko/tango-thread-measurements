75fb1b5abe6dc42dd4a5c4a235770f6c1ea97e05
Zsomething <nothanks@ontheemail.com>
2025-10-14 21:03:23 -0600

    fix wrong benchmark name in Cargo.toml

--- benches/benchb.rs
+++ benches/benchb.rs
@@ -6,20 +6,20 @@ use std::thread;
 
 use std::sync::Arc;
 
-// fn gen_mt_bencher_dummyfunc(num_threads: u64, num_iters: u64) -> impl FnMut(Bencher) -> Box<dyn ErasedSampler>
-// {
-//     move |b: Bencher| {
-//         b.iter(move || {
-//             thread::scope(|scope| {
-//                 for _t in 0..num_threads {
-//                     scope.spawn(move || {
-//                         help_test_dummy_func(num_iters);
-//                     });
-//                 }
-//             });
-//         })
-//     }
-// }
+fn gen_mt_bencher_dummyfunc(num_threads: u64, num_iters: u64) -> impl FnMut(Bencher) -> Box<dyn ErasedSampler>
+{
+    move |b: Bencher| {
+        b.iter(move || {
+            thread::scope(|scope| {
+                for _t in 0..num_threads {
+                    scope.spawn(move || {
+                        help_test_dummy_func(num_iters);
+                    });
+                }
+            });
+        })
+    }
+}
 
 fn gen_st_bencher_dummyfunc(num_iters: u64) -> impl FnMut(Bencher) -> Box<dyn ErasedSampler>
 {
@@ -45,7 +45,6 @@ pub fn dummy_func(maxi: u8, maxj: u8) -> u8 {
 
 pub fn help_test_dummy_func(iters: u64) {
     for _i in 0..iters {
-        //dummy_func(9, 7); // This crashed with heap corruption 3 times out of about 20 runs.
         dummy_func(2, 3);
     }
 }
@@ -55,22 +54,23 @@ fn tangotb_benchmarks() -> impl IntoBenchmarks {
         benchmark_fn("0-threads-1K-iters",
                      gen_st_bencher_dummyfunc(1000)
         ),
-        benchmark_fn("0-threads-1M-iters",
-                     gen_st_bencher_dummyfunc(1_000_000)
+        benchmark_fn("0-threads-10K-iters",
+                     gen_st_bencher_dummyfunc(10_000)
         ),
-//        benchmark_fn("1-threads-1K-iters",
-//                     gen_mt_bencher_dummyfunc(1, 1000)
-//        ),
-//        benchmark_fn("1-threads-1M-iters",
-//                     gen_mt_bencher_dummyfunc(1, 1_000_000)
-//        ),
-//        benchmark_fn("32-threads-1K-iters",
-//                     gen_mt_bencher_dummyfunc(32, 1_000)
-//        ),
-//        benchmark_fn("32-threads-1M-iters",
-//                     gen_mt_bencher_dummyfunc(32, 1_000_000)
-//        ),
-   ]
+        benchmark_fn("0-threads-100K-iters",
+                     gen_st_bencher_dummyfunc(100_000)
+        ),
+
+        benchmark_fn("1-threads-1K-iters",
+                     gen_mt_bencher_dummyfunc(1, 1000)
+        ),
+        benchmark_fn("1-threads-10K-iters",
+                     gen_mt_bencher_dummyfunc(1, 10_000)
+        ),
+        benchmark_fn("1-threads-100K-iters",
+                     gen_mt_bencher_dummyfunc(1, 100_000)
+        ),
+    ]
 }
 
 tango_benchmarks!(tangotb_benchmarks());
