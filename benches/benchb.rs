a686855e779c22415016c41bfba02e5ffaa71cc2
Zsomething <nothanks@ontheemail.com>
2025-10-18 11:00:54 -0600

    oops copy bencha to benchb

--- benches/benchb.rs
+++ benches/benchb.rs
@@ -1,10 +1,6 @@
-#![allow(unused_imports)]
-
 use tango_bench::{Bencher, ErasedSampler, benchmark_fn, tango_benchmarks, tango_main, IntoBenchmarks};
 
 use std::thread;
-
-use std::sync::Arc;
 
 fn gen_mt_bencher_dummyfunc(num_threads: u64, num_iters: u64) -> impl FnMut(Bencher) -> Box<dyn ErasedSampler>
 {
@@ -33,20 +29,23 @@ fn gen_st_bencher_dummyfunc(num_iters: u64) -> impl FnMut(Bencher) -> Box<dyn Er
 use std::hint::black_box;
 #[inline(never)]
 pub fn dummy_func(maxi: u8, maxj: u8) -> u8 {
-    let mut a = Arc::new(0);
+    let mut a = 1;
     for i in 0..maxi {
         for j in 0..maxj {
-            *Arc::make_mut(&mut a) ^= black_box(i.wrapping_mul(j));
+            a ^= black_box(i.wrapping_mul(j));
         }
     }
 
-    *a
+    a
 }
 
-pub fn help_test_dummy_func(iters: u64) {
+pub fn help_test_dummy_func(iters: u64) -> u8 {
+    let mut a = 1;
     for _i in 0..iters {
-        dummy_func(2, 3);
+        a ^= dummy_func(2, 3);
     }
+
+    a
 }
 
 fn tangotb_benchmarks() -> impl IntoBenchmarks {
@@ -76,14 +75,9 @@ fn tangotb_benchmarks() -> impl IntoBenchmarks {
 tango_benchmarks!(tangotb_benchmarks());
 
 use tango_bench::MeasurementSettings;
-use tango_bench::SampleLengthKind::Flat;
 
 tango_main!(
     MeasurementSettings {
-        sampler_type: Flat,
-        cache_firewall: Some(36864), // For my Apple M4 Max
-        max_iterations_per_sample: 1,
-
         ..Default::default()
     }
 );
