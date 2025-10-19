35833e522fdec34de1495ee3c2b2f2e91c28ebfd
Zsomething <nothanks@ontheemail.com>
2025-10-19 14:18:25 -0600

    remove internal iteration of dummy_func, per https://github.com/bazhenov/tango/issues/52#issuecomment-3419853969

--- benches/bencha.rs
+++ benches/bencha.rs
@@ -2,14 +2,14 @@ use tango_bench::{Bencher, ErasedSampler, benchmark_fn, tango_benchmarks, tango_
 
 use std::thread;
 
-fn gen_mt_bencher_dummyfunc(num_threads: u64, num_iters: u64) -> impl FnMut(Bencher) -> Box<dyn ErasedSampler>
+fn gen_mt_bencher_dummyfunc(num_threads: u64) -> impl FnMut(Bencher) -> Box<dyn ErasedSampler>
 {
     move |b: Bencher| {
         b.iter(move || {
             thread::scope(|scope| {
                 for _t in 0..num_threads {
                     scope.spawn(move || {
-                        help_test_dummy_func(num_iters);
+                        help_test_dummy_func();
                     });
                 }
             });
@@ -17,11 +17,11 @@ fn gen_mt_bencher_dummyfunc(num_threads: u64, num_iters: u64) -> impl FnMut(Benc
     }
 }
 
-fn gen_st_bencher_dummyfunc(num_iters: u64) -> impl FnMut(Bencher) -> Box<dyn ErasedSampler>
+fn gen_st_bencher_dummyfunc() -> impl FnMut(Bencher) -> Box<dyn ErasedSampler>
 {
     move |b: Bencher| {
         b.iter(move || {
-            help_test_dummy_func(num_iters);
+            help_test_dummy_func();
         })
     }
 }
@@ -39,35 +39,18 @@ pub fn dummy_func(maxi: u8, maxj: u8) -> u8 {
     a
 }
 
-pub fn help_test_dummy_func(iters: u64) -> u8 {
-    let mut a = 1;
-    for _i in 0..iters {
-        a ^= dummy_func(21, 22);
-    }
-
-    a
+pub fn help_test_dummy_func() -> u8 {
+    1 ^ dummy_func(21, 22)
 }
 
 fn tangotb_benchmarks() -> impl IntoBenchmarks {
     [
-        benchmark_fn("0-threads-1K-iters",
-                     gen_st_bencher_dummyfunc(1000)
-        ),
-        benchmark_fn("0-threads-10K-iters",
-                     gen_st_bencher_dummyfunc(10_000)
-        ),
-        benchmark_fn("0-threads-100K-iters",
-                     gen_st_bencher_dummyfunc(100_000)
+        benchmark_fn("0-threads-1-iters",
+                     gen_st_bencher_dummyfunc()
         ),
 
-        benchmark_fn("1-threads-1K-iters",
-                     gen_mt_bencher_dummyfunc(1, 1000)
-        ),
-        benchmark_fn("1-threads-10K-iters",
-                     gen_mt_bencher_dummyfunc(1, 10_000)
-        ),
-        benchmark_fn("1-threads-100K-iters",
-                     gen_mt_bencher_dummyfunc(1, 100_000)
+        benchmark_fn("1-threads-1-iters",
+                     gen_mt_bencher_dummyfunc(1)
         ),
     ]
 }
