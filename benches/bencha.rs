4b496144b04e48b7d8baa63472e17b42c573d9c9
Zsomething <nothanks@ontheemail.com>
2025-10-27 19:36:34 -0600

    avoid the compiler unrolling the whole dummy_func by making it u64

--- benches/bencha.rs
+++ benches/bencha.rs
@@ -15,7 +15,7 @@ fn gen_mt_bencher_dummyfunc() -> impl FnMut(Bencher) -> Box<dyn ErasedSampler>
     }
 }
 
-pub fn dummy_func(maxi: u8, maxj: u8) -> u8 {
+pub fn dummy_func(maxi: u64, maxj: u64) -> u64 {
     let mut a = 1;
     for i in 0..maxi {
         for j in 0..maxj {
@@ -27,8 +27,8 @@ pub fn dummy_func(maxi: u8, maxj: u8) -> u8 {
 }
 
 use std::hint::black_box;
-pub fn help_test_dummy_func() -> u8 {
-    black_box(dummy_func(3, 2))
+pub fn help_test_dummy_func() -> u64 {
+    black_box(dummy_func(black_box(2520), black_box(2530)))
 }
 
 fn tangotb_benchmarks() -> impl IntoBenchmarks {
