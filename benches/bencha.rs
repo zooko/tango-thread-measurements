76937b5e76c122c0b0d2d4838bc6614112ecacbf
Nothanks Onthelegiblenames <nothanks@ontheemail.com>
2025-10-29 21:22:43 +0000

    put back dummy_func to take and return u8's

--- benches/bencha.rs
+++ benches/bencha.rs
@@ -1,7 +1,5 @@
 use tango_bench::{Bencher, ErasedSampler, benchmark_fn, tango_benchmarks, tango_main, IntoBenchmarks};
 
-use std::thread;
-
 fn gen_st_bencher_dummyfunc() -> impl FnMut(Bencher) -> Box<dyn ErasedSampler> {
     move |b: Bencher| {
         b.iter(move || {
@@ -10,7 +8,7 @@ fn gen_st_bencher_dummyfunc() -> impl FnMut(Bencher) -> Box<dyn ErasedSampler> {
     }
 }
 
-pub fn dummy_func(maxi: u64, maxj: u64) -> u64 {
+pub fn dummy_func(maxi: u8, maxj: u8) -> u8 {
     let mut a = 1;
     for i in 0..maxi {
         for j in 0..maxj {
@@ -22,8 +20,8 @@ pub fn dummy_func(maxi: u64, maxj: u64) -> u64 {
 }
 
 use std::hint::black_box;
-pub fn help_test_dummy_func() -> u64 {
-    black_box(dummy_func(black_box(2520), black_box(2530)))
+pub fn help_test_dummy_func() -> u8 {
+    black_box(dummy_func(3, 2))
 }
 
 fn tangotb_benchmarks() -> impl IntoBenchmarks {
